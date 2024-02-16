2d5bd1a4e4a1740e16a886af942388c568967380
Phil Wekullo <philwekullo@gmail.com>
2024-02-16 08:12:21 +0300

    Fix conflicts

--- src/components/JobItem.jsx
+++ src/components/JobItem.jsx
@@ -1,25 +1,4 @@
-import React from 'react';
-
-
-const JobItem = ({ job, onApply, onRemove }) => { // Added onApply and onRemove props
-  // Extracting text content from HTML
-  const dirtyText = job.text;
-  const parser = new DOMParser();
-  const htmlDoc = parser.parseFromString(dirtyText, 'text/html');
-  // Extracting text content from the parsed HTML
-  const text = htmlDoc.body.textContent;
-
-  return (
-    <div className='job-card'>
-      <h2>{job.role}</h2>
-      <p>{job.company_name}</p>
-      <p>{job.location}</p>
-      <p>{text}</p>
-      <div className="job-btns">
-        <button onClick={onApply}>Apply</button> {/* Added onApply click handler */}
-        <button onClick={onRemove}>Remove</button> {/* Added onRemove click handler */}
-      </div>
-
+import React from "react";
 const JobItem = ({ job, setSelectedJob }) => {
   const handleClick = () => {
     setSelectedJob(job);
